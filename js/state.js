8a337ef848a267d1871fc5392d8478d1e07ea9e4
MeineBuecher <christian.mayer@online.de>
2026-04-01 15:29:40 +0200

    Update state.js

--- js/state.js
+++ js/state.js
@@ -1,1 +1,23 @@
+export let currentRoom = null;
+export let currentRoomOwner = null;
+export let currentParticipantName = null;
+export let currentParticipantStatus = null;
 
+export let currentRole = "participant";
+
+export let currentExpertInviteToken = null;
+export let currentExpertInviteLink = "";
+export let currentExpertSession = null;
+
+export let localSharedScreens = {};
+export let remoteScreenStreams = {};
+
+export let peerConnections = {};
+export let handledSignalIds = new Set();
+
+export let screenSlots = [
+  { title: "Hauptscreen", owner: null, stream: null, active: false },
+  { title: "Screen 2", owner: null, stream: null, active: false },
+  { title: "Screen 3", owner: null, stream: null, active: false },
+  { title: "Screen 4", owner: null, stream: null, active: false }
+];
