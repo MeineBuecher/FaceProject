0018f8a441844fa021258f06884783154251e96f
MeineBuecher <christian.mayer@online.de>
2026-04-01 13:28:02 +0200

    Update app.js

--- app.js
+++ app.js
@@ -39,7 +39,18 @@ let handledSignalIds = new Set();
 const RTC_CONFIG = {
   iceServers: [
     { urls: "stun:stun.l.google.com:19302" },
-    { urls: "stun:stun1.l.google.com:19302" }
+    { urls: "stun:stun1.l.google.com:19302" },
+
+    {
+      urls: "turn:openrelay.metered.ca:80",
+      username: "openrelayproject",
+      credential: "openrelayproject"
+    },
+    {
+      urls: "turn:openrelay.metered.ca:443",
+      username: "openrelayproject",
+      credential: "openrelayproject"
+    }
   ]
 };
 
