c105707c6209574f20f91cc5f394f65cf3d4681f
MeineBuecher <christian.mayer@online.de>
2026-04-01 15:29:55 +0200

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,1 +1,30 @@
+export function setStatus(text) {
+  const el = document.getElementById("status");
+  if (el) el.innerText = text;
+  console.log(text);
+}
 
+export function escapeHtml(value) {
+  return String(value ?? "")
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#39;");
+}
+
+export function formatCurrency(value) {
+  return Number(value || 0).toLocaleString("de-DE", {
+    style: "currency",
+    currency: "EUR"
+  });
+}
+
+export function generateToken(length = 24) {
+  const chars = "ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz23456789";
+  let token = "";
+  for (let i = 0; i < length; i++) {
+    token += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return token;
+}
