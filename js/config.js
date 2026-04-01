7d176410ad2276e65f738cd86429094c6eda11a8
MeineBuecher <christian.mayer@online.de>
2026-04-01 15:29:25 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,14 @@
+export const SUPABASE_URL = "https://zcpdiudjhzgyqgcsawjc.supabase.co";
+export const SUPABASE_KEY = "sb_publishable_PYWvX53ZCnSqCDL5iGjDgQ_VD-KN85H";
 
+export const STORAGE_BUCKET = "faceproject-files";
+
+export const RTC_CONFIG = {
+  iceServers: [
+    { urls: "stun:stun.l.google.com:19302" },
+    { urls: "stun:stun1.l.google.com:19302" }
+  ]
+};
+
+export const SAVED_NAME_KEY = "faceproject_name";
+export const SAVED_ROOM_KEY = "faceproject_room";
