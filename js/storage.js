d925a6f5112f24e7ef757bc3053ec6bb54a04341
MeineBuecher <christian.mayer@online.de>
2026-04-01 15:31:27 +0200

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,1 +1,8 @@
+import { client } from "./supabase.js";
+import { STORAGE_BUCKET } from "./config.js";
 
+export async function uploadFile(file, path) {
+  return await client.storage
+    .from(STORAGE_BUCKET)
+    .upload(path, file);
+}
